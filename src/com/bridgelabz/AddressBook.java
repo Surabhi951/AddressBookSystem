170c21c3de1fc68b26ea5d84e2736bc38cb60882
Surabhi951 <surabhibhagat95@gmail.com>
2022-12-13 20:24:56 +0530

    Modified: Search person in a city or a state

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -6,15 +6,12 @@ import java.util.Scanner;
 
 public class AddressBook {
 
-    Scanner sc = new Scanner(System.in);
-    static HashMap<String,ArrayList> addressBookList = new HashMap<>();// create an object of hashmap
-    static ArrayList <ContactPerson> currentAddressBook;// declare variable
+    static Scanner sc = new Scanner(System.in);
+    static HashMap<String,ArrayList<ContactPerson>> addressBookList = new HashMap<>();// create an object of hashmap
+    static ArrayList<ContactPerson> currentAddressBook;// declare variable
     static String currentAddressBookName;//declare variable
 
-    /*
-    create contacts
-     */
-    public ContactPerson createContact() {
+    public ContactPerson createContact(){
         ContactPerson person = new ContactPerson();//creating object of ContactPerson class
         System.out.print("Enter First Name: ");
         person.setFirstName(sc.next());//using object reference calling setFirstName method to set first name
@@ -64,7 +61,7 @@ public class AddressBook {
         boolean isContactFound = false;
         System.out.println("Enter Name to edit Contact");
         String name = sc.next();
-        for (ContactPerson contactPerson : currentAddressBook) {
+        for (ContactPerson contactPerson : currentAddressBook) { //iterate over the arraylist
             if (name.equalsIgnoreCase(contactPerson.getFirstName())) {
                 isContactFound = true;
                 System.out.print("Enter First Name :");
@@ -95,10 +92,10 @@ public class AddressBook {
     }
 
     /*
-    Delete contact using person`s name
-    if contact found then delete that contact
-     if no contact found then message will be display as oops....contact not found
-     */
+     Delete contact using person`s name
+     if contact found then delete that contact
+      if no contact found then message will be display as oops....contact not found
+      */
     public void deleteContact(){
         boolean isContactFound = false;
         System.out.println("enter name to delete contact");
@@ -122,9 +119,9 @@ public class AddressBook {
     }
 
     /*
-    add multiple address book
-    each address book has unique name
-     */
+   add multiple address book
+   each address book has unique name
+    */
     void addNewAddressBook(){
         System.out.println("Enter name for AddressBook: ");
         String addressBookName = sc.next();
@@ -164,5 +161,51 @@ public class AddressBook {
             System.out.println(person);
         }
     }
+
+    /*
+    search contact by city and state
+     */
+    void searchContact() {
+        System.out.println("1.Search by City \n2.Search by State");
+        int option = sc.nextInt();
+        switch (option) {
+            case 1:
+                System.out.println("Enter city :");
+                searchByCity(sc.next());
+                break;
+            case 2:
+                System.out.println("Enter State :");
+                searchByState(sc.next());
+                break;
+            default:
+                searchContact();
+                break;
+        }
+    }
+
+    //search contact by city
+    void searchByCity(String city) {
+        System.out.println("Search Result: ");
+        for (String addressBookName : addressBookList.keySet()) {//returns a set view of the keys contained in map
+            //forEach iterate through every element of the stream
+            addressBookList.get(addressBookName).stream().forEach((person) -> {
+                if (person.getCity().equalsIgnoreCase(city))
+                    System.out.println(person);//we will get contacts whose city is same
+            });
+        }
+    }
+
+    //search contact by state
+    void searchByState(String state) {
+        System.out.println("Search Result: ");
+        for (String addressBookName : addressBookList.keySet()) {//returns a set view of the keys contained in map
+            //forEach iterate through every element of the stream
+            addressBookList.get(addressBookName).stream().forEach((person) -> {
+                if (person.getState().equalsIgnoreCase(state))
+                    System.out.println(person);//we will get contacts whose state is same
+            });
+        }
+    }
+
 }
 
--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -13,7 +13,7 @@ public class AddressBookMain {
         while (flag1) {
             System.out.println("*************\n"+addressBook.addressBookList.keySet());
             System.out.println("current AddressBook Name: "+addressBook.currentAddressBookName);
-            System.out.println("************\nSelect Option :\n1.Add Contact\n2.Edit Contact\n3.Delete Contact\n4.Display contact\n5.Add New Address Book\n6.Select Address Book\n7.Exit");
+            System.out.println("************\nSelect Option :\n1.Add Contact\n2.Edit Contact\n3.Delete Contact\n4.Display contact\n5.Add New Address Book\n6.Select Address Book\n7.Search contact\n8.Exit");
             int option = sc.nextInt();
             switch (option) { //select option
                 case 1:
@@ -36,6 +36,9 @@ public class AddressBookMain {
                     addressBook.selectAddressBook();
                     break;
                 case 7:
+                    addressBook.searchContact();
+                    break;
+                case 8:
                     flag1 = false;
                     break;
                 default:
